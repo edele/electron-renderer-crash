191ec1495b4884d5d1a19d395eb13822da478faa
Edele Gizatullin <me@edele.ru>
2019-05-13 21:29:33 +0500

    Add flags for debugging

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const { logMemory } = require("./logMemory.js");
 let arr = [];
 
 setInterval(e => {
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 5000; i++) {
     arr.push("x".repeat(100 * 1024 * 1024));
   }
 
